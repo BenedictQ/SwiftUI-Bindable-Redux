567ee38d8a3dfc3f2446ebf397313b9acbbab1e5
Benedict Quinn <bquinn@scottlogic.com>
2020-01-28 11:08:41 +0000

    Extend explanation of default initialize function

--- Sources/BindableSwiftUIRedux/Protocols/ReduxState.swift
+++ Sources/BindableSwiftUIRedux/Protocols/ReduxState.swift
@@ -4,7 +4,8 @@ import Combine
 /// A store will have an associated state property of type ReduxState.
 ///
 /// The state will be initialized with the store, allowing custom binding and publishers to dispatch actions.
-/// The default implementation of initialize does nothing as it will be entirely implementation dependent.
+/// The default implementation of initialize does nothing as it will be entirely implementation dependent, and
+/// not requiring initialization is the simplest, and quite likely, case.
 @available(iOS 13.0, *)
 public protocol ReduxState: ObservableObject {
     associatedtype Store: ReduxStore
