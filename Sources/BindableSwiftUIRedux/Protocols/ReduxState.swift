d8b4d9af2f241cf75e443a19eadaf14e5f6ca1d5
Benedict Quinn <bquinn@scottlogic.com>
2020-01-29 10:33:24 +0000

    Add comment on ReduxState saying it must be class bound

--- Sources/BindableSwiftUIRedux/Protocols/ReduxState.swift
+++ Sources/BindableSwiftUIRedux/Protocols/ReduxState.swift
@@ -6,6 +6,8 @@ import Combine
 /// The state will be initialized with the store, allowing custom binding and publishers to dispatch actions.
 /// The default implementation of initialize does nothing as it will be entirely implementation dependent, and
 /// not requiring initialization is the simplest, and quite likely, case.
+///
+/// State must be class bound at this stage to integrate with SwiftUI state management.
 @available(iOS 13.0, *)
 public protocol ReduxState: AnyObject {
     associatedtype Store: ReduxStore
