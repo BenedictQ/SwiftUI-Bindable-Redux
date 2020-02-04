0b56c75e89ea1410437a90aa093bced52c49e4ba
Benedict Quinn <bquinn@scottlogic.com>
2020-02-04 14:06:36 +0000

    Remove special case for single compose

--- Sources/BindableSwiftUIRedux/Helpers/Composer.swift
+++ Sources/BindableSwiftUIRedux/Helpers/Composer.swift
@@ -3,11 +3,6 @@ public typealias DispatchWrapper = ((@escaping Dispatch) -> Dispatch)
 
 enum Composer {
     static func compose(_ dispatches: [DispatchWrapper]) -> DispatchWrapper {
-        if dispatches.count == 1 {
-            return { (dispatch: @escaping Dispatch) in
-                return dispatches[0](dispatch)
-            }
-        }
         let initial: DispatchWrapper = { dispatch in return dispatch }
         return dispatches.reduce(initial) { (a: @escaping DispatchWrapper, b: @escaping DispatchWrapper) in
             return { (dispatch: @escaping Dispatch) in
