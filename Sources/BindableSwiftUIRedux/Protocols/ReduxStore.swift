7f7ecd9de2b2dd65607768778c12cbc08d2b375a
Benedict Quinn <bquinn@scottlogic.com>
2020-02-04 13:36:11 +0000

    Special case handle only one middleware
    Using reduce only works on arrays with 2 or more elements

--- Sources/BindableSwiftUIRedux/Protocols/ReduxStore.swift
+++ Sources/BindableSwiftUIRedux/Protocols/ReduxStore.swift
@@ -85,6 +85,11 @@ extension ReduxStore {
     }
 
     static func compose(_ dispatches: [DispatchWrapper]) -> DispatchWrapper {
+        if dispatches.count == 1 {
+            return { (dispatch: @escaping Dispatch) in
+                return dispatches[0](dispatch)
+            }
+        }
         return dispatches.reduce(dispatches[0]) { (a: @escaping DispatchWrapper, b: @escaping DispatchWrapper) in
             return { (dispatch: @escaping Dispatch) in
                 return a(b(dispatch))
